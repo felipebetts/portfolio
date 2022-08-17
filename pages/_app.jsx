799cd386fbcc2ebab66d259995b0e0d3c141f4e9
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-08-17 19:44:07 -0300

    fix analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,9 +3,22 @@ import Layout from '../components/layout/Main'
 import theme from '../libs/theme'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '../libs/gtag'
 
 const App = ({ Component, pageProps, router }) => {
-    // src="https://www.googletagmanager.com/gtag/js?id=G-7SVKVB47N2"
+    const nextRouter = useRouter()
+    useEffect(() => {
+        const handleRouteChange = url => {
+            gtag.pageview(url)
+        }
+        nextRouter.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            nextRouter.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [nextRouter.events])
+
     return (
         <ChakraProvider theme={theme}>
             <Fonts />
