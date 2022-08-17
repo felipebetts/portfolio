d8473175b5032ca31ea2cf2fd873cd06cdccc9ed
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-08-17 19:23:39 -0300

    remove previous analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,37 +3,18 @@ import Layout from '../components/layout/Main'
 import theme from '../libs/theme'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
-import Head from 'next/head'
-import Script from 'next/script'
 
 const App = ({ Component, pageProps, router }) => {
+    // src="https://www.googletagmanager.com/gtag/js?id=G-7SVKVB47N2"
     return (
-        <>
-            <Head>
-                <Script
-                    strategy="lazyOnload"
-                    src="https://www.googletagmanager.com/gtag/js?id=G-7SVKVB47N2"
-                />
-                <Script strategy="lazyOnload">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag() {dataLayer.push(arguments)}
-                        gtag('js', new Date());
-                        gtag('config', 'G-7SVKVB47N2', {
-                            page_path: window.location.pathname
-                        });
-                    `}
-                </Script>
-            </Head>
-            <ChakraProvider theme={theme}>
-                <Fonts />
-                <Layout router={router}>
-                    <AnimatePresence exitBeforeEnter initial={true}>
-                        <Component {...pageProps} key={router.route} />
-                    </AnimatePresence>
-                </Layout>
-            </ChakraProvider>
-        </>
+        <ChakraProvider theme={theme}>
+            <Fonts />
+            <Layout router={router}>
+                <AnimatePresence exitBeforeEnter initial={true}>
+                    <Component {...pageProps} key={router.route} />
+                </AnimatePresence>
+            </Layout>
+        </ChakraProvider>
     )
 }
 
