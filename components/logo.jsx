f732320fbf95115241dabc6a77a334310e523b99
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-08-18 14:55:33 -0300

    css fix

--- components/logo.jsx
+++ components/logo.jsx
@@ -5,36 +5,35 @@ import styled from '@emotion/styled'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 
 const LogoBox = styled.span`
-font-weight: bold;
-font-size: 18px;
-display: inline-flex;
-align-items: center;
-height: 30px;
-line-height: 30px;
-padding: 10px;
+    font-weight: bold;
+    font-size: 18px;
+    display: inline-flex;
+    align-items: center;
+    line-height: 30px;
+    padding: 10px;
 
-&:hover img {
-    transform: rotate(-20deg);
-}
+    &:hover img {
+        transform: rotate(-20deg);
+    }
 `
 
 const Logo = () => {
     const footPrintImg = `/images/code${useColorModeValue('_dark', '')}.png`
 
     return (
-        <Link href='/'>
+        <Link href="/">
             <a>
                 <LogoBox>
                     <Image
                         src={footPrintImg}
                         width={20}
                         height={15}
-                        alt='logo'
+                        alt="logo"
                     />
                     <Text
                         color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                        fontFamily='M PLUS Rounded 1c'
-                        fontWeight='bold'
+                        fontFamily="M PLUS Rounded 1c"
+                        fontWeight="bold"
                         ml={3}
                     >
                         Felipe Betts
--- pages/works.jsx
+++ pages/works.jsx
@@ -1,9 +1,4 @@
-import {
-    Container,
-    Heading,
-    SimpleGrid,
-    Divider
-} from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
@@ -12,27 +7,19 @@ import thumbValepay from '../public/images/valepay/login.png'
 import thumbOrcamentoExpress from '../public/images/orcamento_express/cover.png'
 import Layout from '../components/layout/article'
 
-
 const Works = () => {
     return (
-        <Layout>
+        <Layout title="Felipe Betts | Works">
             <Container>
-                <Heading
-                    as='h3'
-                    fontSize={20}
-                    my={4}
-                >
+                <Heading as="h3" fontSize={20} my={4}>
                     Works
                 </Heading>
 
-                <SimpleGrid
-                    columns={[1, 1, 2]}
-                    gap={6}
-                >
+                <SimpleGrid columns={[1, 1, 2]} gap={6}>
                     <Section>
                         <WorkGridItem
-                            id='valepay'
-                            title='Valepay'
+                            id="valepay"
+                            title="Valepay"
                             thumbnail={thumbValepay}
                         >
                             Digital bank
@@ -40,8 +27,8 @@ const Works = () => {
                     </Section>
                     <Section delay={0.1}>
                         <WorkGridItem
-                            id='palmdoctor'
-                            title='PalmDoctor'
+                            id="palmdoctor"
+                            title="PalmDoctor"
                             thumbnail={thumbPalmDoctor}
                         >
                             Health Tech Platform
@@ -49,8 +36,8 @@ const Works = () => {
                     </Section>
                     <Section delay={0.2}>
                         <WorkGridItem
-                            id='orcamento_express'
-                            title='Orçamento Express'
+                            id="orcamento_express"
+                            title="Orçamento Express"
                             thumbnail={thumbOrcamentoExpress}
                         >
                             House Construction Budget Simulator
