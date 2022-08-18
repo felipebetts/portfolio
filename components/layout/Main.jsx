dbf43f50acbe4b2afe044527c6f7bcfb38f2dfd9
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-08-17 21:13:28 -0300

    seo enhancements

--- components/layout/Main.jsx
+++ components/layout/Main.jsx
@@ -1,41 +1,26 @@
-import { Box, Container } from "@chakra-ui/react"
-import Head from "next/head"
+import { Box, Container } from '@chakra-ui/react'
+import Head from 'next/head'
 import Navbar from '../navbar'
 import VoxelDog from '../voxel-dog'
-import NoSsr from "../no-ssr"
-import ThreeBox from "../three/Box"
-import ThreePlane from "../three/Plane"
-import ThreeContainer from "../three/ThreeContainer"
-import ThreeSphere from "../three/Sphere"
+import NoSsr from '../no-ssr'
+import ThreeBox from '../three/Box'
+import ThreePlane from '../three/Plane'
+import ThreeContainer from '../three/ThreeContainer'
+import ThreeSphere from '../three/Sphere'
+import { NextSeo } from 'next-seo'
 
-
-const Main = ({
-    children,
-    router
-}) => {
+const Main = ({ children, router }) => {
     return (
-        <Box as='main' pb={8}>
-            <Head>
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <title>Felipe Betts</title>
-            </Head>
-
+        <Box as="main" pb={8}>
             <Navbar path={router.path} />
-
-            <Container
-                maxW='container.md'
-                pt={14}
-            >
+            <Container maxW="container.md" pt={14}>
                 <NoSsr>
                     {/* <VoxelDog /> */}
                     <ThreeContainer>
                         <ThreeSphere />
                     </ThreeContainer>
                 </NoSsr>
-                <Box
-                    position='relative'
-                    top='50vh'
-                >
+                <Box position="relative" top="50vh">
                     {children}
                 </Box>
             </Container>
--- pages/personal_projects/nft_marketplace.jsx
+++ pages/personal_projects/nft_marketplace.jsx
@@ -1,10 +1,4 @@
-import {
-    Container,
-    Badge,
-    Link,
-    List,
-    ListItem
-} from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { WorkImage, Meta, ProjectTitle } from '../../components/work'
 import P from '../../components/paragraph'
@@ -12,61 +6,61 @@ import Layout from '../../components/layout/article'
 
 const NFTMarketplace = () => {
     return (
-        <Layout
-            title='NFTMarketplace'
-        >
-            <Container>
-                <ProjectTitle>
-                    NFT Marketplace
-                </ProjectTitle>
-                <WorkImage
-                    src='/images/nft_marketplace/1.png'
-                    alt='Valepay'
-                />
-                <P>
-                    Tokenizei is a Fullstack Marketplace of NFTs, built using Next.js on the Ethereum network with Polygon*. Smart contracts were made in solidity and compiled with hardhat. The cryptocurrency used in the transactions is from the Polygon network (MATIC), and the dApp interacts with the MetaMask wallet.
-                </P>
-                <P>
-                    This project was based on a tutorial. During the construction of the project, I was able to develop several web 3.0 concepts and create dApps. I explored Solidity for the first time, creating the contracts and interacting with the blockchain. I learned a lot in this development, hope you like it!
-                    *The network currently used is still the test network, the Mumbai Testnet. The Polygon Mainnet network is not yet being used
-                </P>
-                <List
-                    ml={4}
-                    my={4}
-                >
-                    <ListItem>
-                        <Meta>
-                            LINK
-                        </Meta>
-                        <Link href='https://tokenizei.vercel.app/'>
-                            https://tokenizei.vercel.app/ <ExternalLinkIcon mx='2px' />
-                        </Link>
-                    </ListItem>
-                    <ListItem>
-                        <Meta>
-                            Source
-                        </Meta>
-                        <Link href='https://github.com/felipebetts/nft-marketplace'>
-                            Github <ExternalLinkIcon mx='2px' />
-                        </Link>
-                    </ListItem>
-                    <ListItem>
-                        <Meta>
-                            Stack
-                        </Meta>
-                        <span>Next.js, Solidity</span>
-                    </ListItem>
-                    <ListItem>
-                        <Meta>
-                            Other tecnologies
-                        </Meta>
-                        <span>
-                            Hardhat, Styled Components
-                        </span>
-                    </ListItem>
-                </List>
-            </Container>
-        </Layout>
+        <>
+            <Layout
+                title="NFT Marketplace"
+                description="A NFT Marketplace personal project. I built this while studying blockchain development. Smart contracts written in Solidity."
+            >
+                <Container>
+                    <ProjectTitle>NFT Marketplace</ProjectTitle>
+                    <WorkImage
+                        src="/images/nft_marketplace/1.png"
+                        alt="Valepay"
+                    />
+                    <P>
+                        Tokenizei is a Fullstack Marketplace of NFTs, built
+                        using Next.js on the Ethereum network with Polygon*.
+                        Smart contracts were made in solidity and compiled with
+                        hardhat. The cryptocurrency used in the transactions is
+                        from the Polygon network (MATIC), and the dApp interacts
+                        with the MetaMask wallet.
+                    </P>
+                    <P>
+                        This project was based on a tutorial. During the
+                        construction of the project, I was able to develop
+                        several web 3.0 concepts and create dApps. I explored
+                        Solidity for the first time, creating the contracts and
+                        interacting with the blockchain. I learned a lot in this
+                        development, hope you like it! *The network currently
+                        used is still the test network, the Mumbai Testnet. The
+                        Polygon Mainnet network is not yet being used
+                    </P>
+                    <List ml={4} my={4}>
+                        <ListItem>
+                            <Meta>LINK</Meta>
+                            <Link href="https://tokenizei.vercel.app/">
+                                https://tokenizei.vercel.app/{' '}
+                                <ExternalLinkIcon mx="2px" />
+                            </Link>
+                        </ListItem>
+                        <ListItem>
+                            <Meta>Source</Meta>
+                            <Link href="https://github.com/felipebetts/nft-marketplace">
+                                Github <ExternalLinkIcon mx="2px" />
+                            </Link>
+                        </ListItem>
+                        <ListItem>
+                            <Meta>Stack</Meta>
+                            <span>Next.js, Solidity</span>
+                        </ListItem>
+                        <ListItem>
+                            <Meta>Other tecnologies</Meta>
+                            <span>Hardhat, Styled Components</span>
+                        </ListItem>
+                    </List>
+                </Container>
+            </Layout>
+        </>
     )
 }
 
