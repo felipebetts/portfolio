6858632908590717772aa1fb1a8af76f7f11f397
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-09-25 20:55:37 -0300

    node auth alura

--- pages/courses.jsx
+++ pages/courses.jsx
@@ -6,6 +6,7 @@ import cs50xThumb from '../public/images/courses/CS50x_certificate.png'
 import cs50PThumb from '../public/images/courses/CS50P_certificate.png'
 import this_is_cs50 from '../public/images/courses/this_is_cs50.png'
 import harvard_logo from '../public/images/courses/harvard_logo.png'
+import alura_logo from '../public/images/courses/alura_logo.png'
 
 const Courses = () => {
     return (
@@ -38,6 +39,16 @@ const Courses = () => {
                             thumbnail={harvard_logo}
                         >
                             Introduction to Programming with Python
+                        </GeneralGridItem>
+                    </Section>
+                    <Section delay={0.2}>
+                        <GeneralGridItem
+                            id="cs50p"
+                            title="Node.js + JWT"
+                            href="/courses/node-auth"
+                            thumbnail={alura_logo}
+                        >
+                            Auth with tokens, email confirmation
                         </GeneralGridItem>
                     </Section>
                 </SimpleGrid>
---
+++ pages/courses/node-auth.jsx
@@ -0,0 +1,129 @@
+import {
+    Badge,
+    Container,
+    Flex,
+    Image,
+    Link,
+    List,
+    ListIcon,
+    ListItem
+} from '@chakra-ui/react'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layout/article'
+import { AddIcon, ExternalLinkIcon } from '@chakra-ui/icons'
+
+const NodeAuth = () => {
+    return (
+        <Layout title="Node Auth">
+            <Container pb={2}>
+                <Title from="Courses" href="/courses">
+                    Node Auth <Badge>2022</Badge>
+                </Title>
+                <Flex w="full" justify="center" my={8}>
+                    <Image
+                        src="/images/courses/alura_logo.png"
+                        alt="cs50x_certificate"
+                        maxWidth="160px"
+                    />
+                </Flex>
+                <P>
+                    These were two sequential courses I took from the brazilian
+                    edtech company Alura. Through them, I learned how to
+                    implement an auth system for a node.js server, using JWT.
+                    During the course, I implemented:
+                </P>
+                <List my={4}>
+                    <ListItem>
+                        <ListIcon>
+                            <AddIcon />
+                        </ListIcon>
+                        Multiple user roles
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon>
+                            <AddIcon />
+                        </ListIcon>
+                        Authenticated routes
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon>
+                            <AddIcon />
+                        </ListIcon>
+                        Access token and refresh token
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon>
+                            <AddIcon />
+                        </ListIcon>
+                        User registration confirmation with email service
+                    </ListItem>
+                </List>
+                <List ml={4} my={6}>
+                    <ListItem>
+                        <Meta>Certificate 1</Meta>
+                        <Link
+                            href="https://cursos.alura.com.br/user/felipe-a-betts/course/node-jwt-autenticacao-tokens/certificate"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Official Certificate <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Website Course 1</Meta>
+                        <Link
+                            href="https://cursos.alura.com.br/course/node-jwt-autenticacao-tokens"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Go! <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Certificate 2</Meta>
+                        <Link
+                            href="https://cursos.alura.com.br/user/felipe-a-betts/course/nodejs-refresh-tokens-confirmacao-cadastro/certificate"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Official Certificate <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+
+                    <ListItem>
+                        <Meta>Website Course 2</Meta>
+                        <Link
+                            href="https://cursos.alura.com.br/course/nodejs-refresh-tokens-confirmacao-cadastro"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Go! <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Stack</Meta>
+                        <span>Javascript, Node.js, Express, JWT, and more</span>
+                    </ListItem>
+                    {/* <ListItem>
+                        <Meta>Blogpost</Meta>
+                        <Link href="/works/example#example">
+                            A post from my blog about this project{' '}
+                            <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem> */}
+                </List>
+                <WorkImage
+                    src="/images/courses/node-auth-certificate-1.png"
+                    alt="cs50x_certificate"
+                />
+                <WorkImage
+                    src="/images/courses/node-auth-certificate-2.png"
+                    alt="cs50x_certificate"
+                />
+            </Container>
+        </Layout>
+    )
+}
+
+export default NodeAuth
