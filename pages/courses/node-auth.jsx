c164deb7a83b2ce07643baa61de7938359579538
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-10-04 14:40:26 -0300

    node-auth

--- pages/courses/node-auth.jsx
+++ pages/courses/node-auth.jsx
@@ -1,7 +1,14 @@
 import {
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
     Badge,
+    Box,
     Container,
     Flex,
+    Heading,
     Image,
     Link,
     List,
@@ -72,41 +79,112 @@ const NodeAuth = () => {
                     </ListItem>
                     <ListItem>
                         <Meta>Stack</Meta>
-                        <span>Javascript, Node.js, Express, JWT, and more</span>
+                        <span>
+                            Javascript, Node.js, Express, Redis, JWT, and more
+                        </span>
                     </ListItem>
                 </List>
                 <P>
                     These were two sequential courses I took from the brazilian
                     edtech company Alura. Through them, I learned how to
-                    implement an auth system for a node.js server, using JWT.
-                    During the course, I implemented:
+                    implement an auth system for a node.js server, using JWT and
+                    Redis. The project was a server for a blog, where users can
+                    register, post and comment. During the course, I
+                    implemented:
                 </P>
-                <List my={4}>
-                    <ListItem>
-                        <ListIcon>
-                            <AddIcon />
-                        </ListIcon>
-                        Multiple user roles
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon>
-                            <AddIcon />
-                        </ListIcon>
-                        Authenticated routes
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon>
-                            <AddIcon />
-                        </ListIcon>
-                        Access token and refresh token
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon>
-                            <AddIcon />
-                        </ListIcon>
-                        User registration confirmation with email service
-                    </ListItem>
-                </List>
+                <Accordion allowMultiple my={8}>
+                    <AccordionItem>
+                        <Heading as="h4">
+                            <AccordionButton>
+                                <Box
+                                    flex="1"
+                                    textAlign="left"
+                                    fontWeight="600"
+                                    fontSize={17}
+                                >
+                                    Access token and refresh token
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </Heading>
+                        <AccordionPanel pb={4}>
+                            Implemented through the creation of an allowlist for
+                            the access tokens and a blocklist for the refresh
+                            tokens. Both of those were created using Redis, so
+                            that it would be readily available when accessed
+                            from auth middlewares.
+                        </AccordionPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <Heading as="h4">
+                            <AccordionButton>
+                                <Box
+                                    flex="1"
+                                    textAlign="left"
+                                    fontWeight="600"
+                                    fontSize={17}
+                                >
+                                    Multiple user roles
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </Heading>
+                        <AccordionPanel pb={4}>
+                            In this case, only editors can write/delete posts,
+                            and everyone can comment. In order to ensure this
+                            business rule, I implemented an auth middleware that
+                            checked if the user role was valid based on the
+                            access token received in the request.
+                        </AccordionPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <Heading as="h4">
+                            <AccordionButton>
+                                <Box
+                                    flex="1"
+                                    textAlign="left"
+                                    fontWeight="600"
+                                    fontSize={17}
+                                >
+                                    Authenticated routes
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </Heading>
+                        <AccordionPanel pb={4}>
+                            Some of the routes can only be accessed if you're
+                            logged in, like commenting on posts. To enforce
+                            this, I used a middleware that checked for the
+                            access token on the authorization header as a Bearer
+                            token. That token contains info about the user that
+                            is used to fetch user data and check whether is
+                            valid.
+                        </AccordionPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <Heading as="h4">
+                            <AccordionButton>
+                                <Box
+                                    flex="1"
+                                    textAlign="left"
+                                    fontWeight="600"
+                                    fontSize={17}
+                                >
+                                    User registration confirmation with email
+                                    service
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </Heading>
+                        <AccordionPanel pb={4}>
+                            Using nodemailer, implemented an email verification
+                            register, in such a way that the user is only 100%
+                            validated after he opens the link with a token sent
+                            to his email. This ensures that the user in fact
+                            owns that email address.
+                        </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
                 <WorkImage
                     src="/images/courses/node-auth-certificate-1.png"
                     alt="node_auth_1_certificate"
