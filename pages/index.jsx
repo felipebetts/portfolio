df98a756fe9743a3ac3c450894254714f4a9e289
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2022-11-30 19:59:05 -0300

    remove padding

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,7 +30,7 @@ const Page = props => {
 
     return (
         <Layout>
-            <Container pb={2} overflowX="visible">
+            <Container pb={2} overflowX="visible" px={0}>
                 <Box
                     borderRadius="lg"
                     bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
