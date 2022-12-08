f05debb16510a7c9819e4242fd7227d2dc82b96c
felipebetts <felipe.betts@gmail.com>
2022-12-08 12:30:27 -0300

    remove px from index

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,7 +31,12 @@ const Page = props => {
 
     return (
         <Layout>
-            <Container pb={2} overflowX="visible">
+            <Container
+                pb={2}
+                // overflowX="visible"
+                px={0}
+                // maxW="container.sm"
+            >
                 <Box
                     borderRadius="lg"
                     bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
